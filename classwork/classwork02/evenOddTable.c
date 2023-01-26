c348bfe08cbc90bd98ed26307cb424c2a7df2b94
irrelevantusername <jhuang33@lion.lmu.edu>
2023-01-25 19:16:24 -0800

    feature creep

--- classwork/classwork02/evenOddTable.c
+++ classwork/classwork02/evenOddTable.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+#define START -10
+#define END 10
+#define TOTAL (END - START + 1)
+#define ODDS (TOTAL/2 + (TOTAL % 2)*(END % 2))
+#define EVENS (TOTAL - ODDS)
+//Note: above will be wrong if END < 0 and odd or if START > END + 1.
+//Square the END % 2 term to fix the former
+
 int main() {
-	for(int i = -10; i <= 10; i++) {
+	for(int i = START; i <= END; i++) {
 		printf("%d\t\t%d\t\t%s\n", i, i*i, i % 2 == 0 ? "EVEN" : "ODD");
 	}
+	printf("# odd numbers = %d\n# even numbers = %d", ODDS, EVENS);
 }
