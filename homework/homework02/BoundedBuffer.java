9f07a8ba43cc10bf27e9442bc5dff8ccdc34bed7
Betram Lalusha <lalushabetm@gmail.com>
2023-02-25 12:15:34 -0800

    adding not done bounded buffer test

--- homework/homework02/BoundedBuffer.java
+++ homework/homework02/BoundedBuffer.java
@@ -1,40 +1,38 @@
+package homework.homework02;
 
- class BoundedBuffer {
-    private Object[] buffer = new Object[20]; // arbitrary size
-    private int numOccupied = 0;
-    private int firstOccupied = 0;
-    /* invariant: 0 <= numOccupied <= buffer.length
-       0 <= firstOccupied < buffer.length
-       buffer[(firstOccupied + i) % buffer.length]
-       contains the (i+1)th oldest entry,
-       for all i such that 0 <= i < numOccupied  */
-    public synchronized void insert(Object o)
-      throws InterruptedException
-    {
-      while(numOccupied == buffer.length)
-        // wait for space
-        wait();
-      buffer[(firstOccupied + numOccupied) % buffer.length] = o;
-      numOccupied++;
-      // in case any retrieves are waiting for data, wake them
-      notifyAll();
-  }
-    public synchronized Object retrieve()
-      throws InterruptedException
-    {
-      while(numOccupied == 0)
-        // wait for data
-        wait();
-      Object retrieved = buffer[firstOccupied];
-      buffer[firstOccupied] = null; // may help garbage collector
-      firstOccupied = (firstOccupied + 1) % buffer.length;
-      numOccupied--;
-      // in case any inserts are waiting for space, wake them
-      notifyAll();
-      return retrieved;
+public class BoundedBuffer {
+  private Object[] buffer = new Object[20]; // arbitrary size
+  private int numOccupied = 0;
+  private int firstOccupied = 0;
+  /* invariant: 0 <= numOccupied <= buffer.length
+     0 <= firstOccupied < buffer.length
+     buffer[(firstOccupied + i) % buffer.length]
+     contains the (i+1)th oldest entry,
+     for all i such that 0 <= i < numOccupied  */
+  public synchronized void insert(Object o)
+    throws InterruptedException
+  {
+    while(numOccupied == buffer.length)
+      // wait for space
+      wait();
+    buffer[(firstOccupied + numOccupied) % buffer.length] = o;
+    numOccupied++;
+    // in case any retrieves are waiting for data, wake them
+    notifyAll();
+}
+  public synchronized Object retrieve()
+    throws InterruptedException
+  {
+    while(numOccupied == 0)
+      // wait for data
+      wait();
+    Object retrieved = buffer[firstOccupied];
+    System.out.println("retrieving at " + firstOccupied);
+    buffer[firstOccupied] = null; // may help garbage collector
+    firstOccupied = (firstOccupied + 1) % buffer.length;
+    numOccupied--;
+    // in case any inserts are waiting for space, wake them
+    notifyAll();
+    return retrieved;
   } 
-
-  public static void main(String[] args) {
-    System.out.println("hello there");
-  }
 }
--- homework/homework02/BoundedBufferTest.java
+++ homework/homework02/BoundedBufferTest.java
@@ -3,10 +3,32 @@ package homework.homework02;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/*
+ * First, it might help to reduce the size of the buffer, 
+ * so that all qualitatively different situations can be tested more quickly. 
+ * Second, you need a test program that has multiple threads doing insertions and retrievals, 
+ * with some way to see the difference between when each operation is started 
+ * and when it completes. In the case of the retrievals, 
+ * you will also need to see that the retrieved values are correct. 
+ * Designing such a test program is surprisingly interesting;
+ *  you can have this experience in Programming Project 4.5.
+ */
 public class BoundedBufferTest {
+    BoundedBuffer boundedBuffer = new BoundedBuffer();
     
     @Test
     public void test1() {
         assertEquals(1, 1 + 0);
+        int count = 0;
+        while(count < 20) {
+            try {
+                boundedBuffer.insert(count);
+                Object retrieved = boundedBuffer.retrieve();
+                System.out.println("retrived " + retrieved.toString());
+            }catch(Exception e) {
+                System.out.println("error occured " + e.getMessage());
+            }
+            count++;
+        }
     }
 }
