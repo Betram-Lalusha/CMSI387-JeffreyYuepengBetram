ac1b045e1600321ead96cb6ddfc3c6d777110f94
Betram Lalusha <lalushabetm@gmail.com>
2023-03-07 15:51:12 -0800

    homework02 submission

--- homework/homework02/BoundedBufferTest.java
+++ homework/homework02/BoundedBufferTest.java
@@ -4,14 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /*
- * First, it might help to reduce the size of the buffer, 
- * so that all qualitatively different situations can be tested more quickly. 
- * Second, you need a test program that has multiple threads doing insertions and retrievals, 
- * with some way to see the difference between when each operation is started 
- * and when it completes. In the case of the retrievals, 
- * you will also need to see that the retrieved values are correct. 
- * Designing such a test program is surprisingly interesting;
- *  you can have this experience in Programming Project 4.5.
+ * This class contains tests for both questions 3 and 4.
+ * The test for question 3 is test1() and the test for 
+ * question 4 is test2()
  */
 public class BoundedBufferTest {
     BoundedBuffer boundedBuffer = new BoundedBuffer();
