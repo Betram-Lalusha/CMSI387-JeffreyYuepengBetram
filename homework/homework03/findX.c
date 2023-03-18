d3ef62c18c447844d8b3fa8d8c47183e9eef43cd
Betram Lalusha <lalushabetm@gmail.com>
2023-03-18 16:10:06 -0700

    mmap added

--- homework/homework03/findX.c
+++ homework/homework03/findX.c
@@ -1,8 +1,18 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 
 int main(int argc, char *argv[]) {
+    int c;
     FILE *file;
+    int offset;
+    int pageSize;
+    void *fileAddress;
     if(argc != 2) {
         printf("please include file name.");
         return EXIT_FAILURE;
@@ -11,8 +21,33 @@ int main(int argc, char *argv[]) {
     file = fopen(argv[1],"r");
     if(!file) {
         printf("file %s does not exist",argv[1]);
-        EXIT_FAILURE;
+        return EXIT_FAILURE;
     }
 
+    pageSize = (int)sysconf(_SC_PAGESIZE);
+      if ( pageSize < 0) {
+        perror("sysconf() error");
+        return EXIT_FAILURE;
+      }
+    offset = 0;
+    int fd = open(argv[1],O_CREAT);
+    if ( fd < 0 ) {
+        perror("error opening file. \n");
+        return EXIT_FAILURE;
+    }
+    fileAddress = mmap(NULL,
+                      pageSize,
+                      PROT_READ,
+                      MAP_SHARED,
+                      fd,
+                      offset );
+    if(fileAddress == MAP_FIXED) {
+        printf("error mapping file");
+        return EXIT_FAILURE;
+    }
+
+    printf("file data: %s \n",fileAddress);
+
     fclose(file);
+    return EXIT_SUCCESS;
 }
