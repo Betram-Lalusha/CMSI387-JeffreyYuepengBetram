1c915b5bd6b893d8520da05934550237e81b316e
Betram Lalusha <lalushabetm@gmail.com>
2023-04-01 10:46:03 -0700

    adding solution to Q3

--- homework/homework03/file-process.c
+++ homework/homework03/file-process.c
@@ -2,17 +2,29 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int main() {
+    FILE *file;
+    int currentChar;
     int returnedValue = fork();
     if(returnedValue < 0) {
         printf("error forking");
         return EXIT_FAILURE;
     } else if(returnedValue == 0) {
-        printf("\nopen password file and print out everything in caps\n");
+        file = fopen("/etc/passwd","r");
+        if(file == NULL) {
+            printf("\nFile not found\n");
+            fclose(file);
+            return EXIT_FAILURE;
+        }
+        while((currentChar = fgetc(file)) != EOF) {
+            printf("%c",toupper(currentChar));
+        }
+        fclose(file);
         return EXIT_SUCCESS;
     } else {
         printf("\nNote the parent still has the old standard output.\n");
-        return EXIT_FAILURE;
+        return EXIT_SUCCESS;
     }
 }
