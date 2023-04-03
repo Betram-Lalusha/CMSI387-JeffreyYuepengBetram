9bce6a6dc91124a86159be237111f81b8a89746f
Betram Lalusha <lalushabetm@gmail.com>
2023-04-02 23:20:19 -0700

    changing file extension successful

--- project/03-FinalProduct/personalProject/changeExtension.c
+++ project/03-FinalProduct/personalProject/changeExtension.c
@@ -14,6 +14,7 @@ void fileNotFound(char filePath[]) {
   scanf("%c",&userResponse);
   if(userResponse == 'y') {
     newFile = fopen(filePath,"w");
+    //fputc('h',newFile);
     fclose(newFile);
     printf("\n %s created.\n",filePath);
   } else if (userResponse != 'N') {
@@ -21,10 +22,25 @@ void fileNotFound(char filePath[]) {
     return;
   } 
 }
+
+/**
+ * renames a given  file with the new extension
+*/
+void renameFile(char oldName[],char fileName[], char newExtension[]) {
+  strcat(fileName,newExtension);
+  printf("newName is %s ", oldName);
+  if(rename(oldName,fileName) == 0) {
+    printf("file renamed successfully");
+  } else {
+    printf("error renaming file");
+  }
+}
 int main(int argc, char *argv[])
 {
-   FILE *file;
-   char filePath[1000];
+  FILE *file;
+  int currentChar;
+  char filePath[1000];
+  char fileName[100] = "";
 
   if(argc < 2) {
     printf("\n please include file name.");
@@ -44,6 +60,16 @@ int main(int argc, char *argv[])
     fclose(file);
     return EXIT_FAILURE;
   }
+
+  for(int i = 0; i < strlen(argv[1]);i++) {
+    currentChar = argv[1][i];
+    if(currentChar == '.') {
+      break;
+    } else {
+      strncat(fileName,&currentChar,1);
+    }
+  }
+  renameFile(argv[1],fileName,".java");
   fclose(file);
   return EXIT_SUCCESS;
 }
